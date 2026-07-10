a8eea69b4453298aee70e7bf6258cedef5b49b21
Yagnesh Jariwala <yagneshjariwala-inventyv>
2026-07-10 11:53:04 +0530

    Updated: start with counting program

--- main.go
+++ main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+type model struct {
+	count int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+	case tea.KeyMsg:
+		key := msg.(tea.KeyMsg).String()
+		if key == "ctrl+c" {
+			return m, tea.Quit
+		}
+		if key == "q" {
+			return m, tea.Quit
+		}
+		if key == "up" {
+			m.count++
+		}
+		if key == "down" {
+			m.count--
+		}
+		if key == "r" {
+			m.count = 0
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("Count: %d\n\nctrl+c - Quit • q - Quit • up/down - Increment/Decrement • r - Reset", m.count)
+}
 func main() {
-	store := new(Store)
-	if err := store.Init(); err != nil {
-		log.Fatalf("unable to init store: %v", err)
-	}
-
-	m := NewModel(store)
-
-	p := tea.NewProgram(m)
+	p := tea.NewProgram(model{0})
 	if _, err := p.Run(); err != nil {
-		log.Fatalf("unable to run tui: %v", err)
+		log.Fatal(err)
 	}
+
 }
